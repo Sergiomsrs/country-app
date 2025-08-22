efdb3782d69bb6c483a8e9ea8245c3839455dc70
Sergio <sergiomsrs@gmail.com>
2025-08-22 07:34:34 +0200

    Agregar configuración inicial de Tailwind CSS y componentes de página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomePageComponent,
+    },
+    {
+        path: 'country',
+        loadChildren: () => import('./country/country.routes') //.then( m => m.countryRoutes ) --> No hace falta por que se exporta por defecto
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
+
+
+];
---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+export const countryRoutes: Routes = [
+
+    {
+        path: '',
+        component: ByCapitalPageComponent,
+    },
+/*     {
+        path: 'country',
+        //¿?
+    }, */
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
+];
+
+
+export default countryRoutes;
