ea3861be7b6543081ca06eb37f39495ffafc8ccc
Sergio <sergiomsrs@gmail.com>
2025-08-23 08:57:53 +0200

    Agregar estructura de componentes y diseño para la aplicación de países

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppFooterComponent } from "./shared/components/app-footer/app-footer.component";
+import { TopMenuComponent } from "./country/components/top-menu/top-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppFooterComponent, TopMenuComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
